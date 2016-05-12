b152881955ff00306db458827963c5ac94f97a48
smicl11 <bryan.smith094@gmail.com>
2016-05-12 11:46:19 -0700

    user model, auth, and sessions with basic html views created.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  #User routes
   root to: "users#index"
+  get "/users", to: "users#index", as: "users"
+  get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show", as: "user"
+
+  #Sessions
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to: "sessions#create"
 
 end
