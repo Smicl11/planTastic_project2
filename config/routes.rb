d7132d245075068be4ea56864dca7da2865468d9
relative-rene <rene.arellano415@gmail.com>
2016-05-12 14:20:22 -0700

    comments g & routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'comments/index', to: "comments#index", as: "comments"
+  get 'comments/new', to: "comment#new", as: "comment_new"
+  get 'comments/create', to: "comment#create"
+  get 'comments/edit', to: "comment#edit", as: "comment_edit"
+  get 'comments/update', to: "comment#update", as:
+  get 'comments/destroy', to: "comment#destroy"
+
   #User routes
   root to: "users#index"
   get "/users", to: "users#index", as: "users"
