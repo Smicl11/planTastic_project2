6962eac541043dc5af5e85fce18040deba073ef4
srhbnnstr <srhbnnstr@gmail.com>
2016-05-12 16:24:11 -0700

    adding event views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,20 @@
 class EventsController < ApplicationController
+  def index
+    @event = Event.all
+    render :index
+  end
+
+  def show
+    @event = Event.find_by_id(params[:id])
+    @comments = @event.comments
+    render :show
+  end
+
+  def new
+    redirect_to edit_path(current_user)
+    end
+    @event = Event.new
+  end
+  end
+
 end
