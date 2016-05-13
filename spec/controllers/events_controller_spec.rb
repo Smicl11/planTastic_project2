6f01ab7d60c1eb9de520ab49bc17908eb5a76716
srhbnnstr <srhbnnstr@gmail.com>
2016-05-13 16:11:47 -0700

    rspec still broken

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -3,11 +3,9 @@ require 'rails_helper'
 RSpec.describe EventsController, type: :controller do
     describe "#show" do
       it "renders the :show view" do
-      event = Event.create({title:"Beach Party", location:"Muir Beach", event_date:"12/25/16", event_time:"5:00pm", description:"For Jesus' birthday?" })
+      event = Event.create({title:"Beach Party", location:"Muir Beach", event_date:"12/25/16", event_time:"5:00pm", description:"For Jesus birthday?" })
         get :show, id: event.id
-        expect(response).to render_template(:show)
+        expect(response).to render(:show)
+      end
     end
   end
-
-
-end
