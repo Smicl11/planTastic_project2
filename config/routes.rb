09f0902b4446c8227896b66d0d4f171a4a7dee9b
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-16 10:14:25 -0700

    fixed edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -26,10 +26,10 @@ Rails.application.routes.draw do
   #Events
   get "/events", to: "events#index", as: "events"
   get "/events/new", to: "events#new", as: "new_event"
-  post "/events", to: "events#create", as: "create_event"
+  post "/events", to: "events#create"
   get "/events/:id", to: "events#show", as: "event"
   get "/events/:id/edit", to: "events#edit", as: "edit_event"
-  patch "/events/:id/", to: "events#update", as: "update_event"
-  delete "/events/:id/", to: "events#destroy", as: "delete_event"
+  patch "/events/:id/", to: "events#update"
+  delete "/events/:id/", to: "events#destroy"
 
 end
