44fd266f5ebb5da02553bd049010d8f4fb8bdc80
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-16 10:27:13 -0700

    changed some routes back

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   post "/events", to: "events#create"
   get "/events/:id", to: "events#show", as: "event"
   get "/events/:id/edit", to: "events#edit", as: "edit_event"
-  patch "/events/:id/", to: "events#update"
-  delete "/events/:id/", to: "events#destroy"
+  patch "/events/:id/", to: "events#update", as: "update_event"
+  delete "/events/:id/", to: "events#destroy", as: "delete_event"
 
 end
