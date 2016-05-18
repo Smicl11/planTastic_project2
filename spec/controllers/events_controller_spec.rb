bd93accb5173faa261de2bb2a72d87844e4bfc87
relative-rene <rene.arellano415@gmail.com>
2016-05-18 14:19:07 -0700

    merge save

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,15 +2,13 @@ require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
   describe "GET #new" do
-    context "success" do
-      subject { get :new}
-      it "render the new template" do
-        expect(subject).to render_template(:new)
-      end
-      it "returns a 200 ok status" do
-        get :new
-        expect(response).to have_http_status(:ok)
-      end
+    subject { get :new}
+    it "render the new template" do
+      expect(subject).to render_template(:new)
+    end
+    it "returns a 200 ok status" do
+      get :new
+      expect(response).to have_http_status(:ok)
     end
   end
 
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,12 +5,4 @@ RSpec.describe Event, type: :model do
     it(:location) {should_not == nil}
     it(:event_date) {should_not == nil}
   end
-  context " with 2 or more comments" do
-    it "orders them in chronologically" do
-      event = Event.create!
-      comment1 = event.comments.create!(description: "first comment")
-      comment2 = event.comments.create!(description: "second comment")
-      expect(event.reload.comments).to eq([comment1,comment2])
-    end
-  end
 end
