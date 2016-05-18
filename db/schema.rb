267a9c25e4e2fdeea2362575a1091da09bc3c7f9
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-18 11:17:27 -0700

    username now appears neto a user's comment, but refactoring will be necessary. saving changes for now

--- db/schema.rb
+++ db/schema.rb
@@ -26,12 +26,10 @@ ActiveRecord::Schema.define(version: 20160516180900) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "description"
-    t.string   "contribution"
     t.integer  "user_id"
     t.integer  "event_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-
   end
 
   create_table "events", force: :cascade do |t|
